46d1589931ac502bd4c8d5397c7d32d9238e06c6
ysf98 <98951733+ysf98@users.noreply.github.com>
2023-06-13 19:51:08 +0200

    Update RegistroUsuarios.js

--- reactfront/src/components/RegistroUsuarios.js
+++ reactfront/src/components/RegistroUsuarios.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import {Link} from 'react-router-dom'
 
 
-const api = 'http://localhost:8000/api/register'
+const api = 'http://51.103.67.193/api/register'
 
 const RegistroUsuarios = () => {
 
