1147e3a58a5c3495ef67e697f06c3ce622d69ade
ysf98 <98951733+ysf98@users.noreply.github.com>
2023-06-14 19:22:12 +0200

    Update LoginUsuario.js

--- reactfront/src/components/LoginUsuario.js
+++ reactfront/src/components/LoginUsuario.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import {Link} from 'react-router-dom'
 
-const api = 'http://51.103.67.193:8000/api/login'
+const api = 'https://51.103.67.193:8000/api/login'
 
 const LoginUsuario = () => {
 
